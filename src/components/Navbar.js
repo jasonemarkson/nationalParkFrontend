a62e2dc0408200b386eb20adcfa9c8778453939b
jasonemarkson <jasonmarkson76@gmail.com>
2021-09-14 09:50:14 -0400

    added some more basic css styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Navbar(props) {
     return (
-        <div>
+        <div id="navbar">
             <Link to='/'>
                 <button id="home">Home</button>
             </Link>
