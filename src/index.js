bdbb4ff12e8c5ec228d0ee14bca502d1392690b3
jasonemarkson <jasonmarkson76@gmail.com>
2021-09-20 12:19:19 -0500

    removed localstorage

--- src/index.js
+++ src/index.js
@@ -8,21 +8,12 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import { rootReducer } from './reducers/RootReducer'
-import { loadState, saveState } from './localStorage'
 
-const persistedState = loadState();
 const store = createStore(
   // 1. reducer 2. dev tools
   rootReducer,
-  persistedState,
   composeWithDevTools(applyMiddleware(thunk))
 )
-
-store.subscribe(() => {
-  saveState({
-    savedParks: store.getState().savedParks
-  });
-});
 
 ReactDOM.render(
   <React.StrictMode>
